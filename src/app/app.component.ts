5c1f4726a3e3ac3cc5603e97d00602176b234534
nazligenc <nazalo1907@gmail.com>
2024-06-11 19:11:23 +0300

    calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculator';
-}
+  display = '0';
+  isDarkMode = false;
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.documentElement.classList.toggle('dark-mode', this.isDarkMode);
+  }
+
+
+
+
+  calculate() {
+    try {
+      this.display = eval(this.display.replace('×', '*').replace('÷', '/')).toString();
+    } catch (e) {
+      this.display = 'Error';
+    }
+  }
+  appendNumber(num: number) {
+    if (this.display === '0') {
+      this.display = String(num);
+    } else {
+      this.display += String(num);
+    }
+  }
+  appendOperator(operator: string) {
+    if (operator === '+/-') {
+      this.display = this.display.startsWith('-') ? this.display.substring(1) : '-' + this.display;
+    } else {
+      this.display += operator;
+    }
+  }
+  clear(){
+
+  }
+
+
+
+  }
+
+
+
